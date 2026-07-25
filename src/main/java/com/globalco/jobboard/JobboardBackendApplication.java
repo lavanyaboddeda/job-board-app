97a44527a76851be7b829ad52658a9ac5a51d2f3
Boddeti Lavanya <bl86333@corp.cyient.com>
2026-07-25 10:58:09 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/globalco/jobboard/JobboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.globalco.jobboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobboardBackendApplication.class, args);
+	}
+
+}
